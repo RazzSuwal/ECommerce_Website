69a33bb0acce19443bada8debef27a5e0903e18d
Raxxz <83151947+Raxxz@users.noreply.github.com>
2023-02-23 17:09:47 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+
+<body class="hold-transition skin-blue layout-top-nav">
+    <div class="wrapper">
+    <?php include 'includes/navbar.php'; ?>
+    <div class="container">
+        <h1 class ="text-light">About Us</h1>
+        </div>
+        <?php include 'includes/bigfooter.php'; ?>
+        <?php include 'includes/footer.php'; ?>
+    </div>
+</body>
