cc479ea60d57a5e54874cc91eaaf5b6159049d75
Raxxz <83151947+Raxxz@users.noreply.github.com>
2023-02-23 17:38:53 +0545

    Add files via upload

---
+++ includes/bigfooter.php
@@ -0,0 +1,128 @@
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">What We Do?</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Privacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Physcial Shop</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3><b>eD </b>Shop</h3>
+                        <p>This website sells quality electronic devices like destop, laptop, tables, moblie phones etc.</p>
+                    </div>
+                    <div class="col item social">
+                        <a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+                        <a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+                        <a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+                        <a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
+                        <a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+                    </div>
+                </div>
+                <p class="copyright">eD Shop © 2023</p>
+            </div>
+        </footer>
+    </div>
+
+    <style>
+        .footer-dark {
+            padding:50px 0;
+            color:#f0f9ff;
+            background-color:#282d32;
+            }
+
+            .footer-dark h3 {
+            margin-top:0;
+            margin-bottom:12px;
+            font-weight:bold;
+            font-size:16px;
+            }
+
+            .footer-dark ul {
+            padding:0;
+            list-style:none;
+            line-height:1.6;
+            font-size:14px;
+            margin-bottom:0;
+            }
+
+            .footer-dark ul a {
+            color:inherit;
+            text-decoration:none;
+            opacity:0.6;
+            }
+
+            .footer-dark ul a:hover {
+            opacity:0.8;
+            }
+
+            @media (max-width:767px) {
+            .footer-dark .item:not(.social) {
+                text-align:center;
+                padding-bottom:20px;
+            }
+            }
+
+            .footer-dark .item.text {
+            margin-bottom:36px;
+            }
+
+            @media (max-width:767px) {
+            .footer-dark .item.text {
+                margin-bottom:0;
+            }
+            }
+
+            .footer-dark .item.text p {
+            opacity:0.6;
+            margin-bottom:0;
+            }
+
+            .footer-dark .item.social {
+            text-align:center;
+            }
+
+            @media (max-width:991px) {
+            .footer-dark .item.social {
+                text-align:center;
+                margin-top:20px;
+            }
+            }
+
+            .footer-dark .item.social > a {
+            font-size:20px;
+            width:36px;
+            height:36px;
+            line-height:36px;
+            display:inline-block;
+            text-align:center;
+            border-radius:50%;
+            box-shadow:0 0 0 1px rgba(255,255,255,0.4);
+            margin:0 8px;
+            color:#fff;
+            opacity:0.75;
+            }
+
+            .footer-dark .item.social > a:hover {
+            opacity:0.9;
+            }
+
+            .footer-dark .copyright {
+            text-align:center;
+            padding-top:24px;
+            opacity:0.3;
+            font-size:13px;
+            margin-bottom:0;
+            }
+    </style>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; ECommerce Website Made By <a href="#">RAZZ</a></strong>
+    </div>
+</footer>
